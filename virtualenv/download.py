5bffda9ebcf96d769ee839f73f72b6aebc1aa364
Piet Vandenborre <piet.vandenborre@plsoft.be>
2019-11-11 21:37:09 +0100

    download with timer

--- virtualenv/download.py
+++ virtualenv/download.py
@@ -1,21 +1,29 @@
 import sys
 import os
+import time
 
 import LoggerFactory
 import DownloadFolder
 from ConfigUtils import SyncFolderConfig
 
-if (len(sys.argv) != 3):
-    print('Usage: download <category> <origin>')
+if (len(sys.argv) != 4):
+    print('Usage: download <category> <origin> <seconds>')
     sys.exit(1)
 
 progname = sys.argv[0]
 category = sys.argv[1]
 origin = sys.argv[2]
+seconds = int(sys.argv[3])
 
 localSyncFolderConfig = SyncFolderConfig('local', category, 'in')
 cloudSyncFolderConfig = SyncFolderConfig('cloud', category, origin)
 
 logger = LoggerFactory.createLogger("download", os.path.join(localSyncFolderConfig.subfolder, "download.log"))
 
-DownloadFolder.download(localSyncFolderConfig, cloudSyncFolderConfig)
+while (True):
+    start = time.time()
+    DownloadFolder.download(localSyncFolderConfig, cloudSyncFolderConfig)
+    stop = time.time()
+    delta = stop - start
+    if (delta < seconds):
+        time.sleep(seconds - delta)
